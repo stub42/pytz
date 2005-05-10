fcb20a534eacd3bc9257710cd42eb59df75f2f0f
Arch Librarian <arch@canonical.com>
2005-05-10 20:56:29 +0000

    Can't just use two tzinfo's per timezone - need one for... Author: zenzen Date: 2004-06-02 22:27:24 GMT Can't just use two tzinfo's per timezone - need one for each (utcoffset, dstoffset, tzname) so we can cope with a region using two or more distinct non-dst timezones in sequence, such as CST6CDT in 1935/1936

--- tz/tzinfo.py
+++ tz/tzinfo.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 '''
-$Id: tzinfo.py,v 1.5 2004/06/02 19:39:54 zenzen Exp $
+$Id: tzinfo.py,v 1.6 2004/06/02 22:27:25 zenzen Exp $
 '''
 
-__rcs_id__  = '$Id: tzinfo.py,v 1.5 2004/06/02 19:39:54 zenzen Exp $'
-__version__ = '$Revision: 1.5 $'[11:-2]
+__rcs_id__  = '$Id: tzinfo.py,v 1.6 2004/06/02 22:27:25 zenzen Exp $'
+__version__ = '$Revision: 1.6 $'[11:-2]
 
 from datetime import datetime, timedelta, tzinfo
 from bisect import bisect_right
@@ -79,70 +79,36 @@ class DstTzInfo(BaseTzInfo):
     _zone = None
 
     _tzinfos = None
+    _my_transition_info = None
     _is_dst = False
 
-    def __init__(self, _shadow=None):
-        if _shadow is None:
-            _shadow = self.__class__(self)
-            self._tzinfos = [self, _shadow]
+    def __init__(self, _inf=None, _tzinfos=None):
+        if _inf:
+            self._tzinfos = _tzinfos
+            self._utcoffset, self._dst, self._tzname = _inf
         else:
-            self._is_dst = True
+            _tzinfos = {}
+            self._tzinfos = _tzinfos
+            for inf in self._transition_info:
+                if not _tzinfos.has_key(inf):
+                    _tzinfos[inf] = self.__class__(inf, _tzinfos)
 
     def fromutc(self, dt):
         dt = dt.replace(tzinfo=None)
-        idx = max(0, bisect_right(self._utc_transition_times, dt) -1)
-        inf = self._transition_info[idx]
-        return (dt + inf[0]).replace(tzinfo=self._tzinfos[bool(inf[1])])
-
-    def _lastTransition(self, dt):
-
-        # Locate the nearest transition point to dt
-        dt = dt.replace(tzinfo=None)
         idx = max(0, bisect_right(self._utc_transition_times, dt) - 1)
-
-        # However, dt might have been off since the transition point
-        # is in UTC (up to 12 hours out) and DST offsets might affect it.
-        # The correct transition point is the one closest in time to dt,
-        # which will be idx-1, idx or idx+1
-        options = [
-            (abs(dt - self._utc_transition_times[idx + i]), i)
-            for i in range(-1, 2)
-            if 0 <= idx + i < len(self._utc_transition_times)
-            and bool(self._transition_info[idx + i][1]) == self._is_dst
-            ]
-        offset = min(options)[1]
-
-        inf = self._transition_info[idx + offset]
-        if not bool(self._transition_info[idx + offset][1]) == self._is_dst:
-            err = 'Nearest to %r is %r. Options %r, %r, %r. dst is %r' % (
-                    dt, self._utc_transition_times[idx+offset],
-                    self._utc_transition_times[idx-1],
-                    self._utc_transition_times[idx],
-                    self._utc_transition_times[idx+1],
-                    self._is_dst,
-                    )
-            raise RuntimeError, err
-
-        if bool(self._transition_info[idx][1]) == self._is_dst:
-            return self._transition_info[idx]
-
-        # We got the wrong period, due to utc offsets and dst, however
-        # the correct one will be the nearest one.
-        diff1 = abs(dt - self._utc_transition_times[idx-1])
-        diff2 = abs(dt - self._utc_transition_times[idx+1])
-        if diff1 < diff2:
-            return self._transition_info[idx-1]
-        else:
-            return self._transition_info[idx+1]
+        inf = self._transition_info[idx]
+        return (dt + inf[0]).replace(tzinfo=self._tzinfos[inf])
 
     def utcoffset(self, dt):
-        return self._lastTransition(dt)[0]
+        return self._utcoffset
 
     def dst(self, dt):
-        return self._lastTransition(dt)[1]
+        return self._dst
 
     def tzname(self, dt):
-        return self._lastTransition(dt)[2]
+        return self._tzname
 
     def __repr__(self):
-        return '<DstTzInfo %r, is_dst=%r>' % (self._zone,self._is_dst)
+        return '<DstTzInfo %r %r>' % (
+                self._zone, self._tzname
+                )
