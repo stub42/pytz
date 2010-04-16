8d5b2f70ccf7794a88079c3f832f0150f332b1a7
Stuart Bishop <stuart@stuartbishop.net>
2010-04-16 19:57:19 +0700

    Bump revision numbers to 2010h

--- src/pytz/tests/test_tzinfo.py
+++ src/pytz/tests/test_tzinfo.py
@@ -16,7 +16,7 @@ from pytz.tzinfo import StaticTzInfo
 
 # I test for expected version to ensure the correct version of pytz is
 # actually being tested.
-EXPECTED_VERSION='2010g'
+EXPECTED_VERSION='2010h'
 
 fmt = '%Y-%m-%d %H:%M:%S %Z%z'
 
