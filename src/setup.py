9c73fe4cc1155306359c9983e677382fcf557232
Stuart Bishop <stuart@stuartbishop.net>
2011-03-23 17:42:58 +0700

    Add Python 3 classifier and remove obsolete long description

--- src/setup.py
+++ src/setup.py
@@ -11,23 +11,6 @@ except ImportError:
 
 me = 'Stuart Bishop'
 memail = 'stuart@stuartbishop.net'
-ldesc = """\
-World modern and historical timezone definitions, implemented as
-Python tzinfo subclasses suitable for use my Python's datetime module. 
-Timezone information was provided by the Olson Timezone database.
-Using these timezone definitions resolves all ambiguous daylight savings
-time transitions. All DST trantions have been tested against the reference
-implementation of zdump found in the Olson database to confirm even
-the obscure historical cases work. This test suite is available separatly
-as it is rather large (75558 comparisons), as is the program used
-to generate this package.
-
-The Olson Timezone database is updated several times per year,
-usually with obscure and generally unnoticable changes. These files
-will be regenerated and rereleased soon after updated editions of the
-Olson database are made available.
-"""
-
 packages = ['pytz']
 resources = ['zone.tab', 'locales/pytz.pot']
 for dirpath, dirnames, filenames in os.walk(os.path.join('pytz', 'zoneinfo')):
@@ -63,6 +46,7 @@ setup (
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
     )
