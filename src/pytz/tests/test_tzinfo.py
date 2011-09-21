38a95bd091a41fe7d07fa2f3d219d13725c18a83
Stuart Bishop <stuart@stuartbishop.net>
2011-09-21 18:16:19 +0700

    Bump version number to 2011j

--- src/pytz/tests/test_tzinfo.py
+++ src/pytz/tests/test_tzinfo.py
@@ -21,7 +21,7 @@ from pytz.tzinfo import StaticTzInfo
 
 # I test for expected version to ensure the correct version of pytz is
 # actually being tested.
-EXPECTED_VERSION='2011h'
+EXPECTED_VERSION='2011j'
 
 fmt = '%Y-%m-%d %H:%M:%S %Z%z'
 
