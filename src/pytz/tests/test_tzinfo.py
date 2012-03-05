802a987b8d45bbd787eb10802e633ad9841b64ec
Stuart Bishop <stuart@stuartbishop.net>
2012-03-05 15:34:49 +0700

    Bump version numbers

--- src/pytz/tests/test_tzinfo.py
+++ src/pytz/tests/test_tzinfo.py
@@ -21,7 +21,7 @@ from pytz.tzinfo import DstTzInfo, StaticTzInfo
 
 # I test for expected version to ensure the correct version of pytz is
 # actually being tested.
-EXPECTED_VERSION='2011n'
+EXPECTED_VERSION='2012b'
 
 fmt = '%Y-%m-%d %H:%M:%S %Z%z'
 
