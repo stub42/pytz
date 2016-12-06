4e3b609f67bd31c0ef6dd205e08e5135dfee397e
Stuart Bishop <stuart.bishop@canonical.com>
2016-12-06 16:54:15 +0700

    Bump version numbers to 2016.10/2016j

--- src/pytz/__init__.py
+++ src/pytz/__init__.py
@@ -9,8 +9,8 @@ on how to use these modules.
 '''
 
 # The IANA (nee Olson) database is updated several times a year.
-OLSON_VERSION = '2016g'
-VERSION = '2016.7'  # Switching to pip compatible version numbering.
+OLSON_VERSION = '2016j'
+VERSION = '2016.10'  # Switching to pip compatible version numbering.
 __version__ = VERSION
 
 OLSEN_VERSION = OLSON_VERSION # Old releases had this misspelling
--- src/pytz/tests/test_tzinfo.py
+++ src/pytz/tests/test_tzinfo.py
@@ -21,8 +21,8 @@ from pytz.tzinfo import DstTzInfo, StaticTzInfo
 
 # I test for expected version to ensure the correct version of pytz is
 # actually being tested.
-EXPECTED_VERSION='2016.7'
-EXPECTED_OLSON_VERSION='2016g'
+EXPECTED_VERSION='2016.10'
+EXPECTED_OLSON_VERSION='2016j'
 
 fmt = '%Y-%m-%d %H:%M:%S %Z%z'
 
