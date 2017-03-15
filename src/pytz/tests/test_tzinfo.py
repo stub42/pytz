75bf49433d7a7dac89d3e79095c5c41d88e15d7e
Andreas Stieger <astieger@suse.com>
2017-03-15 22:11:05 +0100

    Bump version numbers to 2017.3/2017a

--- src/pytz/tests/test_tzinfo.py
+++ src/pytz/tests/test_tzinfo.py
@@ -21,8 +21,8 @@ from pytz.tzinfo import DstTzInfo, StaticTzInfo
 
 # I test for expected version to ensure the correct version of pytz is
 # actually being tested.
-EXPECTED_VERSION='2016.10'
-EXPECTED_OLSON_VERSION='2016j'
+EXPECTED_VERSION='2017.3'
+EXPECTED_OLSON_VERSION='2017a'
 
 fmt = '%Y-%m-%d %H:%M:%S %Z%z'
 
