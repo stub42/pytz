024e87765ac37cd5b818347c3e6afa98a1e4f5a6
Stuart Bishop <stuart.bishop@canonical.com>
2018-10-29 17:05:01 +0700

    Bump version numbers to 2018.7./2018g

--- src/pytz/tests/test_tzinfo.py
+++ src/pytz/tests/test_tzinfo.py
@@ -27,8 +27,8 @@ from pytz.tzinfo import DstTzInfo, StaticTzInfo
 
 # I test for expected version to ensure the correct version of pytz is
 # actually being tested.
-EXPECTED_VERSION = '2018.6'
-EXPECTED_OLSON_VERSION = '2018f'
+EXPECTED_VERSION = '2018.7'
+EXPECTED_OLSON_VERSION = '2018g'
 
 fmt = '%Y-%m-%d %H:%M:%S %Z%z'
 
