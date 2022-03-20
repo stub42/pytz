9e15fadcb930d6781591d14a0fb20e1135bde9c0
Stuart Bishop <stuart@stuartbishop.net>
2022-03-20 11:17:11 +1100

    Squashed 'tz/' changes from d2c79c4bc..e6150f292
    95ecc37d2 Release 2022a
    d90452ece * NEWS: Add 2022a "Briefly".
    44b73f770 * NEWS: Coalesce Palestine entries.
    049044d77 * zdump.c: Fix date typo in comment.
    e8681e760 Adjust Chile LMT; expand 1946/1947 commentary
    0caefb646 Revert "Chile’s Magallanes was like Santiago in 1946/7"
    22a2dca72 URL housekeeping
    6d6a21f59 Chile’s Magallanes was like Santiago in 1946/7
    f4b156691 Palestine springs forward a day later
    cac76bf92 Fix white space.
    318759658 Add URLs to Morocco commentary (thanks to Milamber)
    6ff7b3502 Fix French typo
    88d2c3909 Fix Lubumbashi between 1897 and 1929
    7f60edb1d * private.h (UNINIT_TRAP): Add comment.
    db9347fbb Fix processing of TZif truncation
    df4699ddf Add comments on Russia in 1981 (thanks to Alois Treindl)
    4117b20d0 Simplify Ukraine metadata
    93dcac0c6 Ukraine’s 1992/1996 DST changes 02:00s not 01:00u
    b74f54452 dumptime’s argument is always nonnull now
    3a11c9cc2 I18nize zdump a bit better
    a12823400 Improve doc for some source files
    0b8077257 Simplify recent zdump port
    80096e016 zdump ports better to odd C11 platforms
    b6f92f762 zdump -v reports localtime+gmtime failures better
    e3c58df90 * Makefile (AWK): Warn re mawk 1.3.3 (thanks to Jonathan Ben Avraham)
    9c8cb5d76 No leap second on 2022-06-30
    cec7d9e2e Improve rearguard port of post-2087 Morocco
    01eeac6b0 zic: omit parens that the C standard doesn’t allow
    6cfaa796e Fix "el ds" typo in man pages
    aa9c4662f * CONTRIBUTING: Move pull request insns to start.
    4292b9320 Convert CONTRIBUTING back to plain text
    b0c24fad4 Rename CONTRIBUTING.md back to CONTRIBUTING
    7a200f2b3 Fix Y2242 bug in tzdb tarball generation
    5f6467141 Support busybox awk
    8407d785a Port zic to Solaris 10 automounts
    b10ff6d60 Port tzselect to Solaris 10 /bin/sh
    e4a46da99 Simplify tzselect by omitting old Bash workaround
    ee44afb40 Distribute tarballs in ustar rather than gnu form
    68b99887d Remove duplicate Trinidad and Tobago comment
    f6c9f51fa Fix minor formatting inconsistencies
    3fdd3c482 Rename CONTRIBUTING to CONTRIBUTING.md
    b0de1570e Switch to Markdown format
    228a93f75 Remove bad backzone link for US Virgin Is
    8b409e22d Be more generous when dealing with v1 data
    e5a4add00 Fix uninitialized variable bug
    1a4a7eab6 spelling fix
    0b13c6b41 Update Palestine commentary
    59467e337 Improve Palestine predictions for 2025 and later
    b102002e5 Release 2021e
    d86edaf12 Palestine falls back October 29 01:00.
    8da2e33e0 Palestine falls back October 28 24:00
    4de3d7f24 Release 2021d
    951b4f301 * NEWS: Reformat to fit in fewer columns.
    5f06f9a31 zic -r now uses -00 for unspecified timestamps
    f8c660ce6 Simplify zic writezone slightly
    bdeb83438 Document that -00 means local time is unspecified
    f74940d96 Fiji suspends DST for the 2021/2022 season
    0bf5a761f Fix zic.8 typos
    868ba546b Correct spelling of "Treindl" (thanks to Steffen Nurpmeso)
    e5d72c0f9 Clarify comments re Rome in April 1944
    
    git-subtree-dir: tz
    git-subtree-split: e6150f292f19c33ae136f70464d6075c41a1a5f3

--- zic.c
+++ zic.c
@@ -202,6 +202,7 @@ static const char *	progname;
 static ptrdiff_t	timecnt;
 static ptrdiff_t	timecnt_alloc;
 static int		typecnt;
+static int		unspecifiedtype;
 
 /*
 ** Line codes.
@@ -989,9 +990,10 @@ random_dirent(char const **name, char **namealloc)
   char const *src = *name;
   char *dst = *namealloc;
   static char const prefix[] = ".zic";
-  static char const alphabet[] = ("abcdefghijklmnopqrstuvwxyz"
-				  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-				  "0123456789");
+  static char const alphabet[] =
+    "abcdefghijklmnopqrstuvwxyz"
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "0123456789";
   enum { prefixlen = sizeof prefix - 1, alphabetlen = sizeof alphabet - 1 };
   int suffixlen = 6;
   char const *lastslash = strrchr(src, '/');
@@ -1972,6 +1974,10 @@ limitrange(struct timerange r, bool locut, zic_t lo, zic_t hi,
     r.base++;
   }
 
+  /* "-00" before any -r low cutoff.  */
+  if (min_time < lo_time)
+    r.defaulttype = unspecifiedtype;
+
   /* Omit as many initial leap seconds as possible, such that the
      first leap second in the truncated list is <= LO, and is a
      positive leap second if and only if it has a positive correction.
@@ -2120,7 +2126,7 @@ writezone(const char *const name, const char *const string, char version,
 	rangeall.pretrans = rangeall.leapexpiry = false;
 	range64 = limitrange(rangeall, min_time < lo_time,
 			     lo_time, hi_time, ats, types);
-	range32 = limitrange(range64, INT32_MIN < lo_time || want_bloat(),
+	range32 = limitrange(range64, true,
 			     INT32_MIN, INT32_MAX, ats, types);
 
 	/* TZif version 4 is needed if a no-op transition is appended to
@@ -2155,7 +2161,7 @@ writezone(const char *const name, const char *const string, char version,
 		register ptrdiff_t thistimei, thistimecnt, thistimelim;
 		register int	thisleapi, thisleapcnt, thisleaplim;
 		struct tzhead tzh;
-		int currenttype, thisdefaulttype;
+		int thisdefaulttype;
 		bool hicut, pretrans, thisleapexpiry;
 		zic_t lo;
 		int old0;
@@ -2205,13 +2211,12 @@ writezone(const char *const name, const char *const string, char version,
 		  error(_("too many transition times"));
 
 		thistimelim = thistimei + thistimecnt;
-		if (thistimecnt && hi_time < max_time
-		    && ats[thistimelim - 1] == hi_time + 1)
-		  hicut = false;
 		memset(omittype, true, typecnt);
 		omittype[thisdefaulttype] = false;
 		for (i = thistimei - pretrans; i < thistimelim; i++)
 		  omittype[types[i]] = false;
+		if (hicut)
+		  omittype[unspecifiedtype] = false;
 
 		/* Reorder types to make THISDEFAULTTYPE type 0.
 		   Use TYPEMAP to swap OLD0 and THISDEFAULTTYPE so that
@@ -2347,13 +2352,10 @@ writezone(const char *const name, const char *const string, char version,
 		}
 		if (hicut)
 		  puttzcodepass(hi_time + 1, fp, pass);
-		currenttype = 0;
-		for (i = thistimei - pretrans; i < thistimelim; ++i) {
-		  currenttype = typemap[types[i]];
-		  putc(currenttype, fp);
-		}
+		for (i = thistimei - pretrans; i < thistimelim; ++i)
+		  putc(typemap[types[i]], fp);
 		if (hicut)
-		  putc(currenttype, fp);
+		  putc(typemap[unspecifiedtype], fp);
 
 		for (i = old0; i < typecnt; i++) {
 		  int h = (i == old0 ? thisdefaulttype
@@ -2867,6 +2869,9 @@ outzone(const struct zone *zpfirst, ptrdiff_t zonecount)
 	  if (max_year < 2038)
 		max_year = 2038;
 	}
+
+	if (min_time < lo_time || hi_time < max_time)
+	  unspecifiedtype = addtype(0, "-00", false, false, false);
 
 	for (i = 0; i < zonecount; ++i) {
 		struct rule *prevrp = NULL;
@@ -3559,9 +3564,11 @@ mkdirs(char const *argname, bool ancestors)
 		if (mkdir(name, MKDIR_UMASK) != 0) {
 			/* Do not report an error if err == EEXIST, because
 			   some other process might have made the directory
-			   in the meantime.  */
+			   in the meantime.  Likewise for ENOSYS, because
+			   Solaris 10 mkdir fails with ENOSYS if the
+			   directory is an automounted mount point.  */
 			int err = errno;
-			if (err != EEXIST) {
+			if (err != EEXIST && err != ENOSYS) {
 				error(_("%s: Can't create directory %s: %s"),
 				      progname, name, strerror(err));
 				exit(EXIT_FAILURE);
